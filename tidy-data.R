88ff9ea5434ea21c77a4a9c14d474ff2ea5bcc06
RobynIrvine <robyn@poissonconsulting.ca>
2020-01-20 15:28:57 -0800

    starting plots

--- tidy-data.R
+++ tidy-data.R
@@ -4,10 +4,28 @@ sbf_set_sub("clean")
 
 sbf_load_datas()
 
+#bruteforcehistogram  
+killobsdata$DeerStatus<-as.factor(killobsdata$DeerStatus)
+subkill<-killobsdata[killobsdata$DeerStatus == "Killed", ]  
+subobs<-killobsdata[killobsdata$DeerStatus == "Observed",]
+   
+quartz()
+g <- ggplot(subkill, aes(Type))
+g + geom_bar()
+g + geom_bar(aes(fill = DeerSex))
+ggsave("output/DeerSex.png")
 
+quartz()
+g <- ggplot(subkill, aes(Type))
+g + geom_bar()
+g + geom_bar(aes(fill = DeerLifestage))
+ggsave("output/DeerLifeStage.png")
 
-
-
+quartz()
+g<-ggplot(subkill, aes(Island))
+g + geom_bar()
+g + geom_bar(aes(fill = DeerLifestage))
+ggsave("output/DeerLifeStagebyIsland.png")
 
 sbf_set_sub("tidy")
 
