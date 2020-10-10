14b9fbcb61b74cbf45e499816d9bb8631748ad58
RobynIrvine <robyn@poissonconsulting.ca>
2020-10-09 20:19:08 -0700

    joe starting!!

--- clean-data.R
+++ clean-data.R
@@ -4,6 +4,11 @@ sbf_set_sub("input")
 
 sbf_load_datas()
 
+event %<>% 
+  filter(Island == "Ramsay") %>%
+  select(HuntingEventNumber, HuntingType, DateTimeOutingStart, HuntingTimeCalculated) %>%
+  
+
 sbf_set_sub("clean", rm = TRUE)
 
 sbf_save_datas()
