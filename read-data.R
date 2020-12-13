91940beb87e259c0e4c2f0595d81d7c1e3ba3f9c
Joe Thorley <joe@poissonconsulting.ca>
2020-12-13 14:09:49 -0800

    new data

--- read-data.R
+++ read-data.R
@@ -2,8 +2,9 @@ source("header.R")
 
 year <- "2020c"
 
-event <- read_csv(file.path(dir, year, "encounter.csv"))
+event <- read_csv(file.path(dir, year, "event.csv"))
 encounter <- read_csv(file.path(dir, year, "encounter.csv"))
+huntingteam <- read_csv(file.path(dir, year, "huntingteam.csv"))
 
 #saves in directory as objects
 sbf_set_sub("read", rm = TRUE)
