893a1c35af8fda1ace11bd3a8b2e712bd7c9864b
RobynIrvine <robyn@poissonconsulting.ca>
2021-02-16 10:26:29 -0800

    Everything works. Corrected one encounter time and did plots of kills through time for report.

--- plot-deer.R
+++ plot-deer.R
@@ -1,4 +1,5 @@
 source("header.R")
+library (scales)
 
 sbf_set_sub("tidy")
 sbf_load_datas()
@@ -20,15 +21,45 @@ sbf_save_plot(x_name='sexage', caption="Count of animals grouped by age and sex.
 
 
 y<-filter(data, year(DateTimeEncounter)<2018)
+z<-filter(data, year(DateTimeEncounter)>=2018)
+
+
+gp<- ggplot (y, aes(DateTimeEncounter))+
+  geom_histogram(aes(fill=Type), binwidth=1*3600*24*7)+NULL+
+  xlab("Time") + ylab("Deer Killed") +labs(fill="Hunting Method")
+gp <- gp + scale_x_datetime(breaks=date_breaks("1 month"))#, labels = date_format("%d/%m"))
+gp<- gp + limits=c(as.POSIXct('2017/04/15'), as.POSIXct('2017/10/31'))
+sbf_open_window()
+sbf_print(gp)
+sbf_save_plot(x_name='huntingtypethrutime2017', caption="Plot of number of deer killed by hunting method through time for 2017 operations.")
+
+
+gp<- ggplot (y, aes(DateTimeEncounter))+
+  geom_histogram(aes(fill=Type), binwidth=1*3600*24*7)+NULL+
+  xlab("Time") + ylab("Deer Killed") +labs(fill="Hunting Method")
+gp <- gp + scale_x_datetime(breaks=date_breaks("1 month"), labels = date_format("%b"))
+gp <- gp + facet_wrap(~Island)
+sbf_open_window()
+sbf_print(gp)
+sbf_save_plot(x_name='huntypetimebyisl2017', caption="Plot of number of deer killed by hunting method through time by island for 2017 operations.")
+
+
+gp<- ggplot (z, aes(DateTimeEncounter))+
+  geom_histogram(aes(fill=Type), binwidth=1*3600*24*3)+NULL+
+  xlab("Time") + ylab("Deer Killed") +labs(fill="Hunting Method")
+gp <- gp + scale_x_datetime(breaks=date_breaks("1 week"))#, labels = date_format("%d/%m"))
+sbf_open_window()
+sbf_print(gp)
+sbf_save_plot(x_name='huntingtypethrutime2018', caption="Plot of number of deer killed by hunting method through time for 2018 operations.")
+
+
+
+
 gp<- ggplot (y, aes(DateTimeEncounter))+
   geom_histogram(aes(fill=Status))+NULL
 sbf_open_window()
 sbf_print(gp)
 
-gp<- ggplot (y, aes(DateTimeEncounter))+
-  geom_histogram(aes(fill=Type))+NULL
-sbf_open_window()
-sbf_print(gp)
 
 
 gp<- ggplot(data, aes(Type))+
@@ -37,3 +68,10 @@ sbf_open_window()
 sbf_print(gp)
 sbf_save_plot(x_name='agethrutime', caption="Plot of age of deer through time during operations.")
 
+
+#I've had the rubber duck experience and found that the with date the documentation meant specifically an vector of the class Date. The behaviour of binwidthwith the class POSIXct is described in the followup sentence: the bin width of a time variable is the number of seconds.
+
+#In short, the solution is multiplying binwidth by 3600*24 to get days instead of seconds.
+
+#ggplot(df,aes(day)) + 
+ # geom_histogram(binwidth = 1*3600*24,colour = "black",fill = "grey")
--- tidy-data.R
+++ tidy-data.R
@@ -9,5 +9,7 @@ data <- inner_join(encounter, event, by = "HuntingEventNumber")
 event %>% select (HuntingEventNumber, Type, Island,LeadHunter, Dogs, Hunters, Boats, Helicopters,DateTimeOutingStart, DateTimeOutingEnd, DiffTime)
 effdata <- left_join(event, huntingteam, by = "HuntingEventNumber")
 
+
+
 sbf_set_sub("tidy", rm = TRUE)
 sbf_save_datas()
