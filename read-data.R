a7a3187c2bb4c9b08ca0ada0148d1968114b367e
Joe Thorley <joe@poissonconsulting.ca>
2021-03-14 11:22:33 -0700

    master csvs

--- read-data.R
+++ read-data.R
@@ -1,12 +1,10 @@
 source("header.R")
 
-year <- "2020c"
-
 islands <- read_csv("input/islands.csv")
-event <- read_csv("input/event20210313.csv")
-encounter <- read_csv("input/encounter20210313.csv")
-huntingteam <- read_csv("input/huntingteam20210313.csv")
-costs <- read_csv("input/costs20210313.csv")
+event <- read_csv("input/event.csv")
+encounter <- read_csv("input/encounter.csv")
+huntingteam <- read_csv("input/huntingteam.csv")
+costs <- read_csv("input/costs.csv")
 
 #saves in directory as objects
 sbf_set_sub("read", rm = TRUE)
