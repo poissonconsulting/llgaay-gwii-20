fa9e7c3263442110b904fa56a249fc9013d3e17b
Joe Thorley <joe@poissonconsulting.ca>
2021-03-14 10:52:55 -0700

    strating to get rate

--- models-rate.R
+++ models-rate.R
@@ -32,6 +32,13 @@ model <- model("model{
       ePopn[i,j+1] <- ePopn[i,j] - DeerTotal[i,j]
     }
   }
+  sDeerDisperse ~ dnorm(0, 2^-2) T(0,)
+  for(i in 1:nObs) {
+    eDensity[i] <- ePopn[Island[i],Day[i]] / Area[Island[i]]
+    eRate[i] <- 1
+    eDeerDisperse[i] ~ dgamma(sDeerDisperse^-2, sDeerDisperse^-2)
+    Deer[i] ~ dpois(eRate[i] * Hours[i] * eDeerDisperse[i])
+  }
 }",
 modify_data = function(data) {
   data$Day <- data$Day + 1L
@@ -60,6 +67,7 @@ modify_data = function(data) {
                          "Indicator Dog", 
                          "Line Push", "Opportunistic", "Walking")) %>%
     mutate(Type = droplevels(Type))
+  data$nObs <- length(data$Deer)
   data
 },
 gen_inits = function(data) {
@@ -74,7 +82,8 @@ select_data = list(`Day-` = dtt_date(paste("2017-", c("04-21", "10-06"))),
                    Type = factor("Helipcopter", c("Bailing Dog", "Bait Station", "Boat", 
                                      "Helicopter", "Indicator Dog", 
                                      "Line Push", "Opportunistic", "Walking")),
-                   Hours = c(0.05, 14))
+                   Hours = c(0.05, 14),
+                   HourlyRate = c(130, 2400))
 )
 
 sbf_save_block(template(model), "template", caption = "Model description.")
