eb177ed5272e8e53bc3477d7fdb0d32225dc25ff
Joe Thorley <joe@poissonconsulting.ca>
2021-03-13 18:14:17 -0800

    drop hotspring island

--- manipulate-rate.R
+++ manipulate-rate.R
@@ -11,12 +11,14 @@ data <- encounter %>%
   ungroup() %>% 
   right_join(event, by = "HuntingEventNumber") %>%
   replace_na(list(Deer = 0L)) %>%
-  filter(Island %in% c("Ramsay Island", "Murchison Island", "Hotspring Island", "House Island"),
+  filter(Island %in% c("Ramsay Island", "Murchison Island", "House Island"),
          dtt_year(DateTimeOutingStart) == 2017,
          !GridSearch,
          !(OpportunisticHunting & Deer == 0)) %>%
   mutate(Date = dtt_date(DateTimeOutingStart),
-         Island = droplevels(Island),
+         Island = as.character(Island),
+         Island = str_replace(Island, "\\s+Island$", ""),
+         Island = factor(Island, levels = c("Ramsay", "Murchison", "House")),
          Type = as.character(Type),
          Type = if_else(OpportunisticHunting, "Opportunistic", Type),
          Type = factor(Type)) %>%
@@ -27,8 +29,10 @@ sbf_save_data(data)
 
 gp <- ggplot(data = data, aes(x = Date, y = Deer)) +
   facet_wrap(~Type, scales = "free_y") +
-  geom_point(aes(color = Island)) +
+  geom_point(aes(color = Island), alpha = 2/3,
+             position = position_jitter(height = 0.1)) +
   expand_limits(y = 0) +
+  scale_color_manual(values = c("black", "blue", "red")) +
   theme(legend.position = "bottom")
 
 sbf_open_window()
