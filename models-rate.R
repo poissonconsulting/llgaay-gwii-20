70722c17904c46c5df0ec434ce8a558c7afa5e0c
Joe Thorley <joe@poissonconsulting.ca>
2021-03-14 14:27:30 -0700

    fix up rate

--- models-rate.R
+++ models-rate.R
@@ -20,10 +20,9 @@ description %<>% arrange(Parameter)
 sbf_save_table(description, caption = "Parameter descriptions.")
 
 model <- model("model{
-  sPopnDisperse ~ dnorm(0, 2^-2) T(0,)
   for(i in 1:nIsland) {
-    ePopnDisperse[i] ~ dgamma(sPopnDisperse^-2, sPopnDisperse^-2)
-    bPopn1[i] ~ dpois(0.3 * Area[i] * ePopnDisperse[i])
+    bDensity1[i] ~ dunif(0.01, 1.00)
+    bPopn1[i] ~ dpois(Area[i] * bDensity1[i]) T(PopnMin[i],)
   }
   for(i in 1:nIsland) {
     bPopn[i,1] <- bPopn1[i]
@@ -33,14 +32,14 @@ model <- model("model{
     }
   }
   for(i in 1:nType) {
-    bEfficiencyType[i] ~ dnorm(0, 2^-2)
+    bEfficiencyType[i] ~ dnorm(0, 5^-2)
   }
   
   sDeerDisperse ~ dnorm(0, 2^-2) T(0,)
   for(i in 1:nObs) {
     eDensity[i] <- bPopn[Island[i],Day[i]] / Area[Island[i]]
     eEffort[i] <- Hours[i] * HourlyRate[i]
-    log(eEfficiency[i]) <- bEfficiencyType[Type[i]] +  DensityDependent[i] * log(eDensity[i])
+    log(eEfficiency[i]) <- bEfficiencyType[Type[i]] + DensityDependent[i] * log(eDensity[i])
     eDeer[i] <- eEffort[i] * eEfficiency[i] 
     eDeerDisperse[i] ~ dgamma(sDeerDisperse^-2, sDeerDisperse^-2)
     Deer[i] ~ dpois(eDeer[i] * eDeerDisperse[i])
@@ -76,11 +75,17 @@ modify_data = function(data) {
     select(-Island) %>%
     as.matrix()
   
+  data$PopnMin <- data[c("Island", "Deer")] %>%
+    as_tibble() %>%
+    group_by(Island) %>%
+    summarise(Deer = sum(Deer), .groups = "keep") %>%
+    ungroup() %>%
+    use_series("Deer")
+  
   data[c("Island", "Day", "Type", "Deer", "Hours", "HourlyRate", "DensityDependent")] %<>% 
     as_tibble() %>%
-    filter(!Type %in%  c("Opportunistic", "Walking", "Line Push")) %>%
+    filter(!Type %in%  c("Opportunistic", "Walking")) %>%
     mutate(Type = as.character(Type),
-           Type = if_else(str_detect(Type, "Dog"), "Dog", Type),
            Type = factor(Type))
   data$nType <- max(as.integer(data$Type))
   data$nObs <- length(data$Deer)
@@ -96,7 +101,7 @@ select_data = list(`Day-` = dtt_date(paste("2017-", c("04-21", "10-06"))),
                    Island = factor("Ramsay", c("Ramsay", "Murchison", "House")),
                    Area = c(32, 1700),
                    Deer = c(0L, 15L),
-                   Type = factor("Helipcopter", c("Bailing Dog", "Bait Station", "Boat", 
+                   Type = factor("Helicopter", c("Bailing Dog", "Bait Station", "Boat", 
                                      "Helicopter", "Indicator Dog", 
                                      "Line Push", "Opportunistic", "Walking")),
                    Hours = c(0.05, 14),
